8608227ca411f1b4f247d652a9f7443ce3303e0c
Markap <133797030+MarkapToGo@users.noreply.github.com>
2024-05-30 16:26:02 +0200

    Added Mysql support . I guess

--- src/main/java/de/stylelabor/dev/minegamestylelabor/Mine_Game_StyleLabor.java
+++ src/main/java/de/stylelabor/dev/minegamestylelabor/Mine_Game_StyleLabor.java
@@ -14,6 +14,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import java.sql.ResultSet;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -27,7 +29,13 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public final class Mine_Game_StyleLabor extends JavaPlugin implements Listener, TabCompleter {
@@ -37,11 +45,38 @@ public final class Mine_Game_StyleLabor extends JavaPlugin implements Listener, 
     private Location corner2 = null;
     private long lastClickTime = 0;
     private FileConfiguration scoreboardConfig;
+    // Add a new Set to store the players who have bypass protection enabled
+    private final Set<UUID> bypassProtectionPlayers = new HashSet<>();
+    // Add a field for the database connection
+    private Connection connection;
+    private static final Logger LOGGER = Logger.getLogger(Mine_Game_StyleLabor.class.getName());
 
     @Override
     public void onEnable() {
         // Ensures that a config.yml file exists. If it doesn't, the plugin copies the default one included in the JAR file.
         saveDefaultConfig();
+
+        // Load mysql.yml
+        File mysqlFile = new File(getDataFolder(), "mysql.yml");
+        if (!mysqlFile.exists()) {
+            saveResource("mysql.yml", false);
+        }
+        FileConfiguration mysqlConfig = YamlConfiguration.loadConfiguration(mysqlFile);
+
+        // Initialize the database connection
+        try {
+            String hostname = mysqlConfig.getString("hostname");
+            int port = mysqlConfig.getInt("port");
+            String database = mysqlConfig.getString("database");
+            String username = mysqlConfig.getString("username");
+            String password = mysqlConfig.getString("password");
+
+            String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database;
+
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            getLogger().log(Level.SEVERE, "An exception was thrown!", e);
+        }
 
         // Load corners from data.yml
         File dataFile = new File(getDataFolder(), "data.yml");
@@ -57,6 +92,10 @@ public final class Mine_Game_StyleLabor extends JavaPlugin implements Listener, 
             }
         }
 
+
+        // Set up the database
+        setupDatabase();
+
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(this, this);
         Objects.requireNonNull(getCommand("stylelabormine")).setExecutor(this);
@@ -68,6 +107,29 @@ public final class Mine_Game_StyleLabor extends JavaPlugin implements Listener, 
             saveResource("scoreboard.yml", false);
         }
         scoreboardConfig = YamlConfiguration.loadConfiguration(scoreboardFile);
+
+        // Disable weather if the setting is true
+        if (getConfig().getBoolean("disableWeather", false)) {
+            for (World world : getServer().getWorlds()) {
+                world.setWeatherDuration(0);
+                world.setThunderDuration(0);
+                world.setGameRule(GameRule.DO_WEATHER_CYCLE, false);
+            }
+        }
+
+        // Disable time cycle if the setting is true
+        if (getConfig().getBoolean("disableTimeCycle", false)) {
+            for (World world : getServer().getWorlds()) {
+                world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+            }
+        }
+
+        // Disable mob spawn if the setting is true
+        if (getConfig().getBoolean("disableMobSpawn", false)) {
+            for (World world : getServer().getWorlds()) {
+                world.setGameRule(GameRule.DO_MOB_SPAWNING, false);
+            }
+        }
 
         // Register the PlayerJoinEvent
         getServer().getPluginManager().registerEvents(new Listener() {
@@ -158,6 +220,21 @@ public final class Mine_Game_StyleLabor extends JavaPlugin implements Listener, 
                                 }
                             }
                         }
+                    }
+                    return true;
+                }
+
+                if (args[0].equalsIgnoreCase("bypassprotection")) {
+                    if (player.isOp()) {
+                        if (bypassProtectionPlayers.contains(player.getUniqueId())) {
+                            bypassProtectionPlayers.remove(player.getUniqueId());
+                            player.sendMessage("Bypass protection disabled.");
+                        } else {
+                            bypassProtectionPlayers.add(player.getUniqueId());
+                            player.sendMessage("Bypass protection enabled.");
+                        }
+                    } else {
+                        player.sendMessage("You do not have permission to perform this command.");
                     }
                     return true;
                 }
@@ -255,29 +332,39 @@ public final class Mine_Game_StyleLabor extends JavaPlugin implements Listener, 
         }
     }
 
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+
+        // If the player has bypass protection enabled, do not cancel the event
+        if (bypassProtectionPlayers.contains(player.getUniqueId())) {
+            return;
+        }
+
+        // If the player is not an operator, cancel the event
+        event.setCancelled(true);
+    }
+
     public void updateScoreboard(Player player) {
-        System.out.println("Updating scoreboard for player: " + player.getName()); // Debug message
         ScoreboardManager manager = Bukkit.getScoreboardManager();
         if (manager != null) {
             Scoreboard scoreboard = manager.getNewScoreboard();
-            Objective objective = scoreboard.registerNewObjective("StyleLaborMine", Criteria.DUMMY, scoreboardConfig.getString("title", "Scoreboard"), RenderType.INTEGER);
+            String title = ChatColor.translateAlternateColorCodes('&', scoreboardConfig.getString("title", "Scoreboard"));
+            Objective objective = scoreboard.registerNewObjective("StyleLaborMine", Criteria.DUMMY, title, RenderType.INTEGER);
             objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-            List<Map<?, ?>> lines = scoreboardConfig.getMapList("lines");
-            for (int i = 0; i < lines.size(); i++) {
-                Map<?, ?> lineMap = lines.get(i);
-                String text = ChatColor.translateAlternateColorCodes('&', (String) lineMap.get("text"));
-                int updateFrequency = (int) lineMap.get("updateFrequency");
-                Score score = objective.getScore(text);
-                score.setScore(lines.size() - i);
+            // Get the player's current coin balance
+            int coins = getCoins(player);
 
-                // Schedule a repeating task to update the line text
-                new BukkitRunnable() {
-                    @Override
-                    public void run() {
-                        // Update the line text here
-                    }
-                }.runTaskTimer(this, 0, updateFrequency);
+            List<String> lines = scoreboardConfig.getStringList("lines");
+            for (int i = 0; i < lines.size(); i++) {
+                String line = lines.get(i);
+
+                // Replace the placeholder with the player's current coin balance
+                line = line.replace("%stylelabormine_coins%", String.valueOf(coins));
+
+                Score score = objective.getScore(line);
+                score.setScore(lines.size() - i);
             }
 
             player.setScoreboard(scoreboard);
@@ -286,30 +373,35 @@ public final class Mine_Game_StyleLabor extends JavaPlugin implements Listener, 
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        getLogger().info("Block break event triggered");
-
         Player player = event.getPlayer();
         ItemStack itemInHand = player.getInventory().getItemInMainHand();
         if (itemInHand.getType() == Material.DIAMOND_AXE && Objects.requireNonNull(itemInHand.getItemMeta()).getDisplayName().equals(ChatColor.GOLD + "" + ChatColor.BOLD + "StyleLabor Mine - Setup")) {
             event.setCancelled(true);
         }
 
+        // Prevent the block from dropping items
+        event.setDropItems(false);
+
+        // Get the block type
+        Material blockType = event.getBlock().getType();
+
+        // Get the number of coins for the block type from config.yml
+        int coins = getConfig().getInt("blockCoins." + blockType.name(), 0);
+
+        // Give coins to the player
+        giveCoins(player, coins);
+
         Location blockLocation = event.getBlock().getLocation();
-        getLogger().info("Block location: " + blockLocation);
 
         // Check if the broken block is within the specified region
         if (corner1 != null && corner2 != null) {
-            getLogger().info("Corners are not null");
             if (blockLocation.getX() >= Math.min(corner1.getX(), corner2.getX()) && blockLocation.getX() <= Math.max(corner1.getX(), corner2.getX())
                     && blockLocation.getY() >= Math.min(corner1.getY(), corner2.getY()) && blockLocation.getY() <= Math.max(corner1.getY(), corner2.getY())
                     && blockLocation.getZ() >= Math.min(corner1.getZ(), corner2.getZ()) && blockLocation.getZ() <= Math.max(corner1.getZ(), corner2.getZ())) {
 
-                getLogger().info("Block is within the specified region");
-
                 // Get the block types and their percentages from config.yml
                 ConfigurationSection blocksSection = getConfig().getConfigurationSection("blocks");
                 if (blocksSection != null) {
-                    getLogger().info("Block section is not null");
                     List<Material> materials = new ArrayList<>();
                     for (String key : blocksSection.getKeys(false)) {
                         Material material = Material.getMaterial(key);
@@ -321,23 +413,45 @@ public final class Mine_Game_StyleLabor extends JavaPlugin implements Listener, 
 
                     // Replace the broken block with a new block based on the percentages
                     Material material = materials.get(new Random().nextInt(materials.size()));
-                    getLogger().info("New block material: " + material);
 
                     // Schedule a task to set the block after a delay
                     new BukkitRunnable() {
                         @Override
                         public void run() {
                             event.getBlock().setType(material);
-                            getLogger().info("Block type set to new material");
                         }
                     }.runTaskLater(this, getConfig().getLong("regenDelay", 5) * 20); // Convert seconds to ticks
                 }
+            } else {
+                // If the player has bypass protection enabled, do not cancel the event
+                if (bypassProtectionPlayers.contains(player.getUniqueId())) {
+                    return;
+                }
+                // If the block is outside the region, cancel the event
+                event.setCancelled(true);
             }
+        } else {
+            // If the corners are not set, cancel the event
+            event.setCancelled(false);
         }
     }
 
     private String locationToString(Location location) {
         return Objects.requireNonNull(location.getWorld()).getName() + "," + location.getX() + "," + location.getY() + "," + location.getZ() + "," + location.getPitch() + "," + location.getYaw();
+    }
+
+    private int getCoins(Player player) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT coins FROM players WHERE uuid = ?");
+            statement.setString(1, player.getUniqueId().toString());
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getInt("coins");
+            }
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "An exception was thrown!", e);
+        }
+        return 0;
     }
 
     private Location stringToLocation(String string) {
@@ -351,12 +465,44 @@ public final class Mine_Game_StyleLabor extends JavaPlugin implements Listener, 
         return new Location(world, x, y, z, yaw, pitch);
     }
 
+    private void giveCoins(Player player, int amount) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("UPDATE players SET coins = coins + ? WHERE uuid = ?");
+            statement.setInt(1, amount);
+            statement.setString(2, player.getUniqueId().toString());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "An exception was thrown!", e);
+        }
+    }
+
+    private void setupDatabase() {
+        try {
+            // Check if the 'players' table exists
+            ResultSet tables = connection.getMetaData().getTables(null, null, "players", null);
+            if (!tables.next()) {
+                // The 'players' table does not exist, create it
+                PreparedStatement statement = connection.prepareStatement(
+                        "CREATE TABLE players (" +
+                                "uuid VARCHAR(36) NOT NULL," +
+                                "coins INT NOT NULL," +
+                                "PRIMARY KEY (uuid)" +
+                                ")"
+                );
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "An exception was thrown!", e);
+        }
+    }
+
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
         if (command.getName().equalsIgnoreCase("stylelabormine")) {
             if (args.length == 1) {
                 List<String> list = new ArrayList<>();
                 list.add("setup");
+                list.add("bypassprotection"); // Add the new command to the tab completion list
                 return list;
             }
         }
